a71bc1d92fbdfca6736903921e776742929be40e
Justin <zorrotab@gmail.com>
2025-07-22 20:45:45 +1000

    Update lists.hs

--- Examples/lists.hs
+++ Examples/lists.hs
@@ -1,3 +1,20 @@
+import Data.List
+import System.IO
+
+times4 :: Int -> Int
+times4 x = x * 4
+
+multBy4 :: [Int] -> [Int]
+multBy4 [] = []
+multBy4 (x:xs) = times4 x : multBy4 xs
+
+doMult :: (Int -> Int) -> Int -> Int
+doMult func x = func x
+
+doMultOnList :: (Int -> Int) -> [Int] -> [Int]
+doMultOnList func x = map func x
+
+main :: IO ()
 main = do 
    let x = [1..10] 
    let y1 = [head x]
@@ -12,3 +29,11 @@ main = do
    
    putStrLn "The remainings elements of the list is:" 
    print (y2)
+
+   let nums1 = map times4 [1,2,3,4,5]
+   let nums2 = multBy4 nums1
+   putStrLn "[1,2,3,4,5] each element times by 16 is:"
+   print nums2
+
+   print (doMult times4 3)
+   print (doMultOnList times4 [1,2,3])
